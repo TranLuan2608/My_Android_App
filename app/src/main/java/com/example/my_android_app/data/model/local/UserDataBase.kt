2895691f842b1d3f284410ad01ec1ab4418faefd
TranLuan2608 <73928878+TranLuan2608@users.noreply.github.com>
2021-07-26 21:11:05 +0700

    Commit change

--- app/src/main/java/com/example/my_android_app/data/model/local/UserDataBase.kt
+++ app/src/main/java/com/example/my_android_app/data/model/local/UserDataBase.kt
@@ -23,13 +23,11 @@ abstract class UserDataBase: RoomDatabase() {
 open class SingletonHolder <out T : Any, in A>(creator: (A) -> T) {
     private var creator: ((A) -> T)? = creator
     @Volatile private var instance: T? = null
-
     fun getInstance(arg: A): T {
         val i = instance
         if (i != null) {
             return i
         }
-
         return synchronized(this) {
             val i2 = instance
             if (i2 != null) {
--- app/src/main/java/com/example/my_android_app/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/my_android_app/ui/main/view/MainActivity.kt
@@ -1,18 +1,9 @@
 package com.example.my_android_app.ui.main.view
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import com.example.my_android_app.R
-import com.example.mylibrary.FragmentTwo
-import com.example.mylibrary.User
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,12 +15,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //test1()
         //test7()
-        test8()
-
+        //test8()
+//        val arg = arrayOf(1, 2, 3, 4)
+//        test("String")
+//        test(10)
+//        test(arg)
+//        test(2.5)
+        val a = layGiaTriTraVe<Int,String>(2)
+        val b = layGiaTriTraVe<Int,String>(3)
+        //khong
+//        val c = layGiaTriTraVe<Int,Long>(5)
+        Log.d("TagL", "Gia tri tra ve a $a")
+        Log.d("TagL", "Gia tri tra ve b $b")
+//        Log.d("TagL", "Gia tri tra ve b $c")
     }
-
-
-
 }
 //generics là một class có thể implement và có thể thay đổi kiểu dữ liệu tham số truyền vào
 //vì vậy ta có thể truyền vào một list dữ liệu
@@ -39,20 +38,29 @@ class MainActivity : AppCompatActivity() {
 //kieu du lieu tong quat
 //khi hoat dong cua class can co nhieu tham so truyen vao
 
- class MyClass<T>(text: T) {
+fun <T> test(text: T) {
+    println(text.toString())
+}
+//truyen vao mot gia tri bat ky in ra kieu gia tri mong muon
+fun <T, V>layGiaTriTraVe(text: T): V? {
+    return text.toString() as? V
+}
+
+class MyClass<T>(text: T) {
     var name = text
     init {
-        Log.d("TagU","Generics $name")
+        Log.d("TagU", "Generics $name")
     }
 }
-private fun test1(){
+
+private fun test1() {
     var name: MyClass<String> = MyClass("Geeks for Geeks")
     var rank: MyClass<Int> = MyClass(12)
 }
 
 
 open class Father()
-class Son: Father()
+class Son : Father()
 
 // supertype = subtype Covariance
 class Person<out T>(val value: T) {
@@ -60,54 +68,58 @@ class Person<out T>(val value: T) {
         return value
     }
 }
-fun test9()
-{
+
+fun test9() {
     val sonObject = Person(Son())
     val fatherObject: Person<Father>
     fatherObject = sonObject
 
 }
 
-
-//
 class OutClass<out T>(private val value: T) {
     fun get(): T {
         return value
     }
 }
-private fun test2(){
+
+private fun test2() {
     val out = OutClass("string")
     val ref: OutClass<Any> = out
 }
 
-//subtype = supertype Contravariance
+//subtype = supertype
+// Contravariance
 class InClass<in T> {
     fun toString(value: T): String {
         return value.toString()
     }
 }
-private fun test3(){
+
+private fun test3() {
     val inClassObject: InClass<Number> = InClass()
     val ref = inClassObject
 }
 
 //class su dung ca in va out (Invariance)
 fun test4() {
-    //val x: MyClass2<Any> = MyClass2<Int>()        // Error: Type mismatch
+    // val x: MyClass2<Any> = MyClass2<Int>()        // Error: Type mismatch
     val y: MyClass2<out Any> = MyClass2<String>() // Works since String is a subtype of Any
     //val z: MyClass2<in String> = MyClass2<Any>() // Error since Any is a supertype of String
 }
+
 class MyClass2<T>
 
 fun test5() {
     val y: MyClass3<Any> = MyClass3<String>() // Compiles without error
 }
+
 class MyClass3<out T>
 
 fun test6() {
     var a: Container<Dog> = Container<Animal>()  //compiles without error
     //var b: Container<Animal> = Container<Dog>()  //gives compilation error
 }
+
 open class Animal
 class Dog : Animal()
 class Container<in T>
@@ -120,27 +132,28 @@ fun copy(from: Array<out Any>, to: Array<Any>) {
     // printing elements of array in which copied
     for (i in to.indices) {
         var to = to[i]
-        Log.d("TagU","Generics $to")
+        Log.d("TagU", "Generics $to")
     }
 }
+
 fun test7() {
     val ints: Array<Int> = arrayOf(1, 2, 3)
-    val any :Array<Any> = Array<Any>(3) { "" }
+    val any: Array<Any> = Array<Any>(3) { "" }
     copy(ints, any)
 
 }
 
 //Star projection
 fun printArray(array: Array<*>) {
-    array.forEach { Log.d("TagU","Generics $it")
-    }}
-
-fun test8() {
-    val name  = arrayOf(1,"for","Geeks")
-        printArray(name)
+    array.forEach {
+        Log.d("TagU", "Generics $it")
+    }
 }
 
-
+fun test8() {
+    val name = arrayOf(1, "for", "Geeks")
+    printArray(name)
+}
 
 
 //        val edtUserName = findViewById<EditText>(R.id.edtUserName)
@@ -190,10 +203,6 @@ fun test8() {
 //        transaction.commit()
 //
 //
-
-
-
-
 
 
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
